c545d67651925407781c90062aa99afa6ab9dbbf
Herman Athor <hermanathorg@outlook.com>
2023-11-22 18:55:15 +0100

    initial commit of an e-commerce store that i am migrating

---
+++ data/footerData.ts
@@ -0,0 +1,57 @@
+interface Links {
+  title: string;
+  href: string;
+}
+
+export const companyLinks: Links[] = [
+  {
+    title: "About",
+    href: "#",
+  },
+  {
+    title: "Terms of Service",
+    href: "#",
+  },
+  {
+    title: "Privacy Policy",
+    href: "#",
+  },
+  {
+    title: "Cookie Policy",
+    href: "#",
+  },
+];
+
+export const solutionLinks: Links[] = [
+  {
+    title: "Business Line of Credit",
+    href: "#",
+  },
+  {
+    title: "SBA Loan",
+    href: "#",
+  },
+  {
+    title: "Revenue Based Financing",
+    href: "#",
+  },
+  {
+    title: "Invoice Factoring",
+    href: "#",
+  },
+];
+
+export const partnershipLinks: Links[] = [
+  {
+    title: "Loan Partner",
+    href: "#",
+  },
+  {
+    title: "Affiliate",
+    href: "#",
+  },
+  {
+    title: "Brand Guideline",
+    href: "#",
+  },
+];
