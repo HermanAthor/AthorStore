2e9b9f0ae4d616f8e0210817dd122b9499e2b062
Herman Athor <hermanathorg@outlook.com>
2023-11-30 20:23:19 +0100

    added calculated values to the cart summary

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,21 +1,12 @@
 "use client";
 
 import cartState from "@/context/recoilContext";
-import ProductInterface, { Product } from "@/helpers/fetchingData";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 
 const Cart = () => {
-  //   const { title, image, quantity, price } = item;
-  //   const total = price * quantity;
-  //   let photo = "";
-  //   if (image) {
-  //     photo = image;
-  //   } else {
-  //     photo = item.thumbnail;
-  //   }
-
   const [cartItems, setCartItems] = useRecoilState<any>(cartState);
+  const [subTotal, setSubTotal] = useState<number>(0);
 
   useEffect(() => {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
@@ -28,24 +19,18 @@ const Cart = () => {
     }
   }, []);
 
-  // Add items to Cart function
-  // Remove Items from cart
+  useEffect(() => {
+    let totalAmount = 0;
 
-  /*const removeFromCart = (item) => {
-    const isItemInCart = cartItems.find((cartItem) => cartItem.id === item.id);
+    cartItems.forEach((item: any) => {
+      const { quantity, price } = item;
+      totalAmount += price * quantity;
+    });
 
-    if (isItemInCart.quantity === 1) {
-      setCartItems(cartItems.filter((cartItem) => cartItem.id !== item.id)); // if the quantity of the item is 1, remove the item from the cart
-    } else {
-      setCartItems(
-        cartItems.map((cartItem) =>
-          cartItem.id === item.id
-            ? { ...cartItem, quantity: cartItem.quantity - 1 } // if the quantity of the item is greater than 1, decrease the quantity of the item
-            : cartItem
-        )
-      );
-    }
-  };*/
+    setSubTotal(totalAmount);
+  }, [cartItems]);
+  const shippingAmount = 4.99;
+  const grandTotal = subTotal + shippingAmount;
 
   return (
     <>
@@ -54,16 +39,23 @@ const Cart = () => {
         <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
           <div className="rounded-lg md:w-2/3">
             {cartItems?.map((item: any) => {
-              const { id, price, title } = item;
+              const { id, title, quantity, image, price } = item;
+
+              let photo = "";
+              if (image) {
+                photo = image;
+              } else {
+                photo = item.thumbnail;
+              }
               return (
                 <div
                   key={id}
                   className="justify-between mb-6 rounded-lg bg-white dark:bg-gray-600 p-6 shadow-md sm:flex sm:justify-start"
                 >
                   <img
-                    src="https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                    alt="product-image"
-                    className="w-full rounded-lg sm:w-40"
+                    src={photo}
+                    alt={title}
+                    className="w-full rounded-lg sm:w-40 h-36"
                   />
                   <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
                     <div className="mt-5 sm:mt-0">
@@ -83,7 +75,7 @@ const Cart = () => {
                         <input
                           className="h-8 w-8 bg-white dark:bg-gray-700 text-center text-xs outline-none"
                           type="number"
-                          value="2"
+                          value={quantity}
                           min="1"
                         />
                         <button className="cursor-pointer  bg-gray-100 dark:bg-gray-700 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50">
@@ -97,13 +89,13 @@ const Cart = () => {
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
                           viewBox="0 0 24 24"
-                          stroke-width="1.5"
+                          strokeWidth="1.5"
                           stroke="currentColor"
                           className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500"
                         >
                           <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             d="M6 18L18 6M6 6l12 12"
                           />
                         </svg>
@@ -117,17 +109,23 @@ const Cart = () => {
           <div className="mt-6 h-full rounded-lg border bg-white dark:bg-gray-700 p-6 shadow-md md:mt-0 md:w-1/3">
             <div className="mb-2 flex justify-between">
               <p className="text-gray-700 dark:text-gray-100">Subtotal</p>
-              <p className="text-gray-700 dark:text-gray-100">$129.99</p>
+              <p className="text-gray-700 dark:text-gray-100">
+                {subTotal.toFixed(2)}
+              </p>
             </div>
             <div className="flex justify-between">
               <p className="text-gray-700 dark:text-gray-100">Shipping</p>
-              <p className="text-gray-700 dark:text-gray-100">$4.99</p>
+              <p className="text-gray-700 dark:text-gray-100">
+                {shippingAmount}
+              </p>
             </div>
             <hr className="my-4" />
             <div className="flex justify-between">
               <p className="text-lg font-bold">Total</p>
               <div className="">
-                <p className="mb-1 text-lg font-bold">$134.98 USD</p>
+                <p className="mb-1 text-lg font-bold">
+                  {grandTotal.toFixed(2)} USD
+                </p>
                 <p className="text-sm text-gray-700 dark:text-gray-100">
                   including VAT
                 </p>
