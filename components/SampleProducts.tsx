4d765861c0585030167816dc71eb0cabfe0b0208
Herman Athor <hermanathorg@outlook.com>
2023-12-01 23:13:01 +0100

    added swiperjs and modified the SampleProduct component

--- components/SampleProducts.tsx
+++ components/SampleProducts.tsx
@@ -1,4 +1,12 @@
+"use client";
 import ProductInterface from "@/helpers/fetchingData";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/bundle";
+import "swiper/css";
+import "swiper/css/navigation";
+//import "./swipperstyles.css";
+
 import React from "react";
 
 type PropsType = {
@@ -10,7 +18,49 @@ function SampleProducts({ category, product }: PropsType) {
   return (
     <div>
       <div>{category}</div>
-      <div className="carousel carousel-center max-w-screen-xl p-4 space-x-4 bg-white dark:bg-black rounded-box">
+      <Swiper
+        navigation={true}
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        breakpoints={{
+          350: {
+            slidesPerView: 2,
+            spaceBetween: 2,
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 2,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 3,
+          },
+          1024: {
+            slidesPerView: 6,
+            spaceBetween: 5,
+          },
+        }}
+      >
+        {product.map((item) => {
+          return (
+            <SwiperSlide key={item.id}>
+              <div className="card w-96 h-80 bg-base-100 shadow-xl image-full">
+                <figure>
+                  <img src={item.image} alt={item.title} />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">{item.title}</h2>
+                  <p></p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+
+      {/* <div className="carousel carousel-center max-w-screen-xl p-4 space-x-4 bg-white dark:bg-black rounded-box">
         {product.map((item) => (
           <div className="carousel-item">
             <div className="card w-96 h-80 bg-base-100 shadow-xl image-full">
@@ -27,7 +77,7 @@ function SampleProducts({ category, product }: PropsType) {
             </div>
           </div>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
