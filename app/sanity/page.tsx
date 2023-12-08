4687afdcb8a42822bcf9f3a05ddf81a5088d99a6
Herman Athor <hermanathorg@outlook.com>
2023-12-08 23:28:53 +0100

    Modified the Sanity products display component 'still in testing mode'

--- app/sanity/page.tsx
+++ app/sanity/page.tsx
@@ -1,20 +1,32 @@
-import { client } from "@/libs/sanity";
+import ProductCard from "@/components/shared-ui/ProductCard";
+import { client, urlFor } from "@/libs/sanity";
 import React from "react";
 
+// function that query data from sanity studio
 const getData = async () => {
   const query = "*[_type == 'product']";
   const data = await client.fetch(query);
   return data;
 };
 
-const page = async () => {
+const SanityProductsPage = async () => {
+  // Making use of the function that queries data form sanity studio
   const data = await getData();
-  console.log(data);
   return (
     <div>
       <p>Hello there</p>
+      {data.map((product: any) => {
+        const { name, price, ratingRate, ratingCount, images } = product;
+        const image = images.map((img: any) => img);
+        const imageUrl = urlFor(image[0].asset._ref).url();
+        return (
+          <div>
+            <ProductCard product={product} category={"Just Products"} />
+          </div>
+        );
+      })}
     </div>
   );
 };
 
-export default page;
+export default SanityProductsPage;
