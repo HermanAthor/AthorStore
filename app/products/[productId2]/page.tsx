11516ee937e7b95bee0230772fa5b28fe2549b22
Herman Athor <hermanathorg@outlook.com>
2023-12-14 22:27:35 +0100

    conditionally rendered the component that allows you select sizes and colors for fashion categories

--- app/products/[productId2]/page.tsx
+++ app/products/[productId2]/page.tsx
@@ -5,6 +5,7 @@ import MoreProducts from "@/components/MoreProducts";
 import ProductCard from "@/components/ProductCard";
 import { filterProducts } from "@/helpers/filteredProducts";
 import BuyButton from "@/components/cartButtons/BuyButton";
+import SizeAndColorPreference from "@/components/Custom-ui/SizeAndColorPreference";
 
 interface pageProps {
   params: { productId2: number | string };
@@ -35,6 +36,14 @@ const ProductPage2: FC<pageProps> = async ({ params }) => {
     products: products,
     category: category,
   });
+  let isFashion = true;
+  if (category === "women's clothing") {
+    isFashion = true;
+  } else if (category === "men's clothing") {
+    isFashion = true;
+  } else {
+    isFashion = false;
+  }
 
   return (
     <div>
@@ -154,39 +163,8 @@ const ProductPage2: FC<pageProps> = async ({ params }) => {
                 </span>
               </div>
               <p className="leading-relaxed">{description}</p>
-              <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
-                <div className="flex">
-                  <span className="mr-3">Color</span>
-                  <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                  <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-                  <button className="border-2 border-gray-300 ml-1 bg-red-500 rounded-full w-6 h-6 focus:outline-none"></button>
-                </div>
-                <div className="flex ml-6 items-center">
-                  <span className="mr-3">Size</span>
-                  <div className="relative">
-                    <select className="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10">
-                      <option>SM</option>
-                      <option>M</option>
-                      <option>L</option>
-                      <option>XL</option>
-                    </select>
-                    <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                      <svg
-                        fill="none"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        className="w-4 h-4"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M6 9l6 6 6-6"></path>
-                      </svg>
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <div className="flex justify-between items-center">
+              {isFashion && <SizeAndColorPreference />}
+              <div className="flex justify-between items-center py-5">
                 <span className="title-font font-medium dark:text-gray-100 text-2xl text-gray-900">
                   ${price}
                 </span>
