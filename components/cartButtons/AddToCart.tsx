d2b9fc51f3921cda6b11e02e2442408d78ad6034
Herman Athor <hermanathorg@outlook.com>
2023-12-28 19:53:29 +0100

    Made the add to cart button responsive to screen sizes

--- components/cartButtons/AddToCart.tsx
+++ components/cartButtons/AddToCart.tsx
@@ -1,17 +1,44 @@
 "use client";
 import { Product } from "@/helpers/fetchingData";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useCart } from "react-use-cart";
+import { FaCartPlus } from "react-icons/fa";
 
 function AddToCart({ item }: any) {
   const { addItem } = useCart();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Attach event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  const buttonText = windowWidth > 350 ? "Add" : "Add to cart";
+  // const getButtonText = () => {
+  //   let text = "";
+  //   if (windowWidth < 350) {
+  //     return (text = "Add");
+  //   } else if (windowWidth < 357) {
+  //     return (text = "Add to cart");
+  //   }
+  // };
+  // const buttonText = getButtonText();
   return (
     <div>
       <button
         onClick={() => addItem(item)}
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 block md:px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
-        Add to cart
+        <p className="inline md:hidden">Add</p>
+        <p className="hidden md:inline">Add to cart</p>
       </button>
     </div>
   );
